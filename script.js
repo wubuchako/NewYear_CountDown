1d64c8e04664750ad65a3e6630ed8e3485c0ff44
wubuchako <56101474+wubuchako@users.noreply.github.com>
2020-07-07 19:36:53 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const countdown = document.getElementById('countdown');
+
+
+const newYearTime = new Date();
